0e51c8c2aed93a6ad3fb929f9b597ffb5e5102ca
Pujitha <pujithashaik93@gmail.com>
2025-10-18 11:30:37 +0530

    project v1.0

--- backend-springbootapp/src/main/java/com/klef/BackendSpringbootappApplication.java
+++ backend-springbootapp/src/main/java/com/klef/BackendSpringbootappApplication.java
@@ -2,13 +2,20 @@ package com.klef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendSpringbootappApplication {
+public class BackendSpringbootappApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendSpringbootappApplication.class, args);
-		System.out.println("Spring Boot Project is Running ...");
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BackendSpringbootappApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpringbootappApplication.class, args);
+        System.out.println("Spring Boot Project is Running ...");
+    }
 
 }
